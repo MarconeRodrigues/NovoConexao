ad23af1913fef079e250715c829f0d962be615d1
MarconeRodrigues <marcone.rodrigues.dev@outlook.com>
2024-02-15 19:55:37 -0300

    Add project files.

---
+++ NovoConexao/Models/LinksConexao.cs
@@ -0,0 +1,15 @@
+﻿namespace NovoConexao.Models
+{
+    public class LinksConexao
+    {
+        public required string Negocio { get; set; }
+
+        public string? Categoria { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public string? LinkAcesso { get; set; }
+    }
+}
