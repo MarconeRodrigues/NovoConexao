8d449fec17ff33486bf2e5aa920dabd2fb3e2dbc
MarconeRodrigues <marcone.rodrigues.dev@outlook.com>
2024-03-27 16:42:48 -0300

    New Version

---
+++ NovoConexao/Models/MuralAvisos.cs
@@ -0,0 +1,11 @@
+﻿namespace NovoConexao.Models
+{
+    public class MuralAvisos
+    {
+        public string? Titulo { get; set; }
+        public string? ImagemUrl { get; set; }
+        public string? Descricao { get; set; } 
+
+
+    }
+}
---
+++ NovoConexao/Models/Negocio.cs
@@ -0,0 +1,12 @@
+﻿namespace NovoConexao.Models
+{
+    public class Negocio
+    {
+        public string? NameNegocio { get; set; }
+
+        public string? UrlText { get; set; }
+
+        public string? IconNegocio { get; set; }
+
+    }
+}
